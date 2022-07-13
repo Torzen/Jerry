df5d0878b2d81bef8798d1f01e0e01537a1ccd56
Bishal-241 <anjalishahjhau12345@gmail.com>
2022-07-13 15:14:31 +0545

    Added application

--- jerry/jerry/settings.py
+++ jerry/jerry/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
+    'management',
+    'alpha'
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'jerry.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'management/templates',BASE_DIR / "store/templates",BASE_DIR / "alpha/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "alpha/static",
+    BASE_DIR / "store/static",
+    BASE_DIR / "management/static"
+    # '/var/www/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
