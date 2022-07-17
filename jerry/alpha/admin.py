9bf0e7ba3ebbad66b219610e44171a63d4fa5d5d
Bishal-241 <anjalishahjhau12345@gmail.com>
2022-07-17 19:15:20 +0545

    staff registration models

--- jerry/alpha/admin.py
+++ jerry/alpha/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from alpha.models import staffRegistration
 # Register your models here.
+admin.site.register(staffRegistration)
--- jerry/alpha/models.py
+++ jerry/alpha/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class staffRegistration(models.Model):
+    username = models.CharField( max_length=50,null=False)
+    password = models.CharField( max_length = 20,null = False)
+    fullname = models.CharField(max_length=100 ,null = False)
+    email = models.EmailField()
+    address = models.CharField(max_length=200)
+    number = models.CharField(max_length=10)
+    nationality = models.CharField(max_length = 100)
+    photoUrl = models.CharField(max_length = 500)
