29bef011b70011163e499b4eaa05e8338de80611
Bishal-241 <anjalishahjhau12345@gmail.com>
2022-07-26 22:45:09 +0545

    Some changes

--- jerry/alpha/views.py
+++ jerry/alpha/views.py
@@ -1,15 +1,36 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from alpha.models import staffRegistration
 # Create your views here.
 def main(request):
     return HttpResponse("main")
 def about(request):
     return HttpResponse("about")
 def contact(request):
-    return HttpResponse("contact")
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        subject = request.POST.get('subject')
+        email = request.POST.get('email')
+        desc = request.POST.get('desc')
+        a = (f"name:{name},subject:{subject},email:{email},desc:{desc}")
+        return HttpResponse(a)
+    else:
+        return render(request,'contact.html')
+    # return HttpResponse("contact")
 def  register(request):
-    return HttpResponse("register")
+    if request.method  == 'POST':
+        username= request.POST.get('username')
+        password= request.POST.get('password')
+        fullname= request.POST.get('fullname')
+        email= request.POST.get('email')
+        address= request.POST.get('address')
+        number= request.POST.get('number')
+        nationality= request.POST.get('nationality')
+        photoUrl= request.POST.get('photoUrl')
+        user = staffRegistration(username = username,password = password , fullname = fullname , email = email , address = address , number = number , nationality = nationality , photoUrl = photoUrl)
+        user.save()
+    return render(request , 'register.html')
+   
 def alumuni(request):
     return HttpResponse("alumuni")
 def patners(request):
