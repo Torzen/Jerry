d09125f4bedee101fe4b80389d4ae6045ed9896b
Bishal-241 <anjalishahjhau12345@gmail.com>
2022-08-29 11:21:39 +0545

    User-Registration instance db

--- jerry/alpha/model.py
+++ jerry/alpha/model.py
@@ -1,10 +1,8 @@
-import email
-from pyexpat import model
 from django.db import models
 
 # Create your models here.
-class users(model.Model):
+class userRegistrations(models.Model):
     username = models.CharField(max_length=100)
     email = models.EmailField()
-    passwordhash = models.CharField(max_length=200)
+    password = models.CharField(max_length=64)
     
