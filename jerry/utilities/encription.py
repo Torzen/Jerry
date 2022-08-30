87d25c8e332e5b1908c1f63fdadfde1d284fe75b
Bishal-241 <anjalishahjhau12345@gmail.com>
2022-08-30 22:09:01 +0545

    Added encription.py

--- jerry/utilities/encription.py
+++ jerry/utilities/encription.py
@@ -8,15 +8,4 @@ def encript(a):
     return hashed
 
 if __name__ == "__main__":
-    string="we-fix-it"
-    encoded=string.encode()
-    result = hashlib.sha256(encoded)
-    print("String : ", end ="")
-    print(string)
-    print("Hash Value : ", end ="")
-    print(result)
-    print("Hexadecimal equivalent: ",result.hexdigest())
-    print("Digest Size : ", end ="")
-    print(result.digest_size)
-    print("Block Size : ", end ="")
-    print(result.block_size)
+    print(encript('Bishal is great'))
