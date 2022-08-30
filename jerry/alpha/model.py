98a50d74d589c0cd68aac9f76b7b8c621774dc74
Bishal-241 <anjalishahjhau12345@gmail.com>
2022-08-30 22:08:45 +0545

    Create model.py

--- jerry/alpha/model.py
+++ jerry/alpha/model.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class userRegistrations(models.Model):
     username = models.CharField(max_length=100)
-    email = models.EmailField()
+    email =  models.EmailField(unique=True)
     password = models.CharField(max_length=64)
     
+class users(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=64)
